64cc13e3bd11bf14a160c78b6ac73265f0b6a6a4
Alaf Baig <mirzaalaf21@gmai.com>
2025-09-08 20:50:24 +0530

    fetch api

--- fetch_API/fetch_api.js
+++ fetch_API/fetch_api.js
@@ -1,3 +1,13 @@
 
+//fetch a dog breed api 
+let url =  "https://dogapi.dog/api/v2/breeds";
+
+(async ()=>{
+    let response = await fetch(url);
+  let output =  await response.json();
+    console.log(output.data);    
+}
+)();
+
 
 
